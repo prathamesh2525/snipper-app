fff7c717678fccf295baa121189388371c286a66
Prathamesh Madniwale <61145586+prathamesh2525@users.noreply.github.com>
2024-03-18 00:42:29 +0530

    updated database urls

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,13 @@
 const { Sequelize } = require("sequelize")
 
-const sequelize = new Sequelize("mysql", "root", "password", {
-  host: "localhost",
-  dialect: "mysql",
-})
+const sequelize = new Sequelize(
+  "if0_36181564_my_mysql",
+  "if0_36181564",
+  "HeFYazG2O3zN",
+  {
+    host: "sql202.infinityfree.com",
+    dialect: "mysql",
+  }
+)
 
 module.exports = sequelize
